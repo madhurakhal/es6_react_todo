6c10a86317a14c5bccd739c19fc218e51f4d062f
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2015-08-27 20:24:57 +0545

    message changed, let used instead of var.

--- components/ToDo.js
+++ components/ToDo.js
@@ -28,18 +28,20 @@ export default class ToDo extends React.Component{
     });
   }
   render() {
-    var lists = this.state.todos.map((todoItem, idx) => {
+    let lists = this.state.todos.map((todoItem, idx) => {
       return(<li className="ui-state-default" key={todoItem.id}>
               <div className="checkbox">
-                  <label>
-                      <input type="checkbox" defaultChecked={todoItem.completed}
-                      value={todoItem.id}
-                      onChange={this._toggleTodo}
-                      /><span>{todoItem.name}</span>
-                  </label>
+                <label>
+                    <input type="checkbox" defaultChecked={todoItem.completed}
+                    value={todoItem.id}
+                    onChange={this._toggleTodo}
+                    /><span>{todoItem.name}</span>
+                </label>
               </div>
             </li>);
     })
+    let todosLeft = this.state.todos.filter((todo) => {return todo.completed != true}).length;
+    let todoText = ((todosLeft == 1) || (todosLeft == 0)) ? "Todo" : "Todos";
     return (
         <div className="todolist not-done">
           <h1>Todos</h1>
@@ -50,8 +52,8 @@ export default class ToDo extends React.Component{
           </ul>
           <div className="todo-footer">
             <strong>
-              <span className="count-todos">{this.state.todos.filter((todo) => {return todo.completed != true}).length}</span>
-            </strong> Todos Left
+              <span className="count-todos">{todosLeft}</span>
+            </strong> {todoText} Left
           </div>
         </div>
       )
